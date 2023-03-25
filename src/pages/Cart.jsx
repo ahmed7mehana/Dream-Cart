76edae045f52ecb1298ff28378fdff7cfbe9f87b
Ahmed Mehana <102672897+ahmed7mehana@users.noreply.github.com>
2023-03-26 01:23:53 +0200

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,30 +3,12 @@ import {useDispatch,useSelector} from "react-redux"
 import Table from 'react-bootstrap/Table';
 import { Clear, deletefromcart } from '../../rtk/Slices/CartSlice';
 import { Container} from 'react-bootstrap';
+import axios from 'axios';
+import  {checkout}  from '/checkout';
+import Link from 'next/link';
+import products from '/products.json';
 
 function Cart() {
-
-
-
-  const redirectToCheckout = async () => {
-    // Create Stripe checkout
-    const {
-      data: { id },
-    } = await axios.post('/api/checkout_sessions', {
-      items: Object.entries(cartDetails).map(([_, { id, quantity }]) => ({
-        price: id,
-        quantity,
-      })),
-    });
-
-    // Redirect to checkout
-    const stripe = await getStripe();
-    await stripe.redirectToCheckout({ sessionId: id });
-  };
-
-
-
-
 
   const cart = useSelector(state=>state.cart)
 
@@ -35,10 +17,9 @@ function Cart() {
     return acc
     }
   ,0);
-console.log()
-
   const dispatch = useDispatch()
 
+console.log(products.products.buy)
 
   return (
     <div >
@@ -59,6 +40,7 @@ console.log()
     <td style={{ width: '5%', padding:'10' }} ><img style={{ width: '100%' }} src={item.image} /></td>
     <td style={{ width: '10%' , padding:'0' ,textAlign:'center' }} >{item.price} $</td>
     <td style={{ width: '10%', padding:'0' ,textAlign:'center'  }} >{item.quantity} </td>
+    <td style={{ width: '10%', padding:'0' ,textAlign:'center'  }} > <Link className=' uppercase font-bold text-sm' href={products.products[item.id-1].buy}>buy</Link> </td>
     <td style={{ width: '3%', padding:'5px 0px' ,textAlign:'center' }}  ><button className=" bg-slate-400 rounded-3xl    hover:bg-black py-1 px-2 hover:text-white font-bold duration-500"  onClick={()=>{dispatch(deletefromcart(item.id))}}> X</button></td>
 
   </tr>
@@ -69,6 +51,7 @@ console.log()
 </Table>
 <p className=' m-auto mt-10 mb-10 flex justify-center bg-slate-400 rounded-3xl py-1 px-6 w-fit uppercase hover:bg-black hover:text-white font-bold duration-500'>total price ({totalprice.toFixed(2)}$) </p>
 <button className='m-auto mt-10 mb-10 flex justify-center bg-slate-400 rounded-3xl py-1 px-6 w-fit uppercase hover:bg-black hover:text-white font-bold duration-500' onClick={()=>dispatch(Clear())}>Clear</button>
+
 
   </div>
 ):(<div className=' mr-[-75px] mt-[-37.5px] ' style={{position:'absolute' ,top:"50%",left:"50%" ,translate:"-50%"  }}>
